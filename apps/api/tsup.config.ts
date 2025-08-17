661e2898bb8a5ad400d03a0b71772ca9ab563217
Ravikant Saini <ravikantsaini047@gmail.com>
2025-08-17 17:02:44 +0530

    renamed 'nuvix' to 'api'

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -1,8 +1,7 @@
-const InjectCreateRequirePlugin = require('./scripts/InjectCreateRequirePlugin');
-const swcDefaultConfig = require('@nestjs/cli/lib/compiler/defaults/swc-defaults').swcDefaultsFactory().swcOptions;
+import { defineConfig } from 'tsup';
 
 function printStylizedNuvix() {
-const logo = `
+  const logo = `
 ╔════════════════════════════════════════════════════════════════════════════════════════════════╗
 ║████████████████████████████████████████████████████████████████████████████████████████████████║
 ║████████████████████████████████████████████████████████████████████████████████████████████████║
@@ -26,63 +25,24 @@ const logo = `
 ║████████████████████████████████████████████████████████████████████████████████████████████████║
 ╚════════════════════════════════════════════════════════════════════════════════════════════════╝
 `;
-
-
   console.log('\x1b[38;5;208m' + logo + '\x1b[0m');
 }
 
-
-/**
- * @type {import('webpack').Configuration}
- * @param {import('webpack').Configuration} options
- */
-module.exports = (options) => {
-  if (process.env.NODE_ENV !== 'production') {
-    printStylizedNuvix();
-  }
+export default defineConfig(() => {
+  printStylizedNuvix();
   return {
-    ...options,
-    output: {
-      ...options.output,
-      filename: options.output.filename.replace(/\.js$/, '.mjs'),
-    },
-    target: 'node22',
-    experiments: {
-      outputModule: true,
-    },
-    module: {
-      ...options.module,
-      rules: [
-        {
-          test: /\.ts$/,
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: 'swc-loader',
-              options: {
-                ...swcDefaultConfig,
-                jsc: {
-                  ...swcDefaultConfig.jsc,
-                  target: 'es2023',
-                },
-                module: {
-                  type: 'es6',
-                  ignoreDynamic: true,
-                  resolveFully: true,
-                  noInterop: false,
-                  preserveImportMeta: true,
-                },
-                minify: false,
-                sourceMaps: true,
-              },
-            },
-          ],
-        },
-      ],
-    },
-    plugins: [
-      ...options.plugins,
-      new InjectCreateRequirePlugin(),
-    ]
-  }
-};
+    entry: ['src/main.ts'],
+    format: ['cjs', 'esm'],
+    dts: false,
+    sourcemap: true,
+    clean: true,
+    outDir: '../../dist/api',
+    noExternal: [],
+    splitting: false,
+    minify: false,
+    target: 'es2024',
+    skipNodeModulesBundle: true,
+    shims: true,
+    tsconfig: './tsconfig.app.json',
+  };
+});
